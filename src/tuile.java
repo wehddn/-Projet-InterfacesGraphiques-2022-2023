616a16fb08fb5fc36fdf185a0aee56dbbdfa0a02
wehddn <nigraff@yandex.ru>
2023-02-21 11:41:23 +0100

    Tuile

--- src/tuile.java
+++ src/tuile.java
@@ -7,10 +7,35 @@ public class tuile {
     private ArrayList<Integer> connexions; //static?
     private boolean power;
 
+    public int getType() {
+        return type;
+    }
 
+    public void setType(int type) {
+        this.type = type;
+    }
     
+    public char getComposant() {
+        return composant;
+    }
     
-
-
-
+    public void setComposant(char composant) {
+        this.composant = composant;
+    }
+    
+    public ArrayList<Integer> getConnexions() {
+        return connexions;
+    }
+    
+    public void setConnexions(ArrayList<Integer> connexions) {
+        this.connexions = connexions;
+    }
+    
+    public boolean isPower() {
+        return power;
+    }
+    
+    public void setPower(boolean power) {
+        this.power = power;
+    }
 }
