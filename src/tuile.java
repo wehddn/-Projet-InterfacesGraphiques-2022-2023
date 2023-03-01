a93220dc5a7c1701d7bdbac906ff2781a8b5f8cb
wehddn <nigraff@yandex.ru>
2023-03-01 12:22:53 +0100

    Merge tuiles

--- src/tuile.java
+++ src/tuile.java
@@ -6,44 +6,43 @@ public class Tuile {
     private char composant; //S/L/W/.
     private ArrayList<Integer> connexions; //static?
     private boolean power;
+    private boolean visited;
 
     public Tuile(String composant, ArrayList<Integer> connexions) {
         this.composant = composant.charAt(0);
         this.connexions = connexions;
     }
-
+    
     public int getType() {
         return type;
     }
-
     public void setType(int type) {
         this.type = type;
     }
-    
     public char getComposant() {
         return composant;
     }
-    
     public void setComposant(char composant) {
         this.composant = composant;
     }
-    
     public ArrayList<Integer> getConnexions() {
         return connexions;
     }
-    
     public void setConnexions(ArrayList<Integer> connexions) {
         this.connexions = connexions;
     }
-    
     public boolean isPower() {
         return power;
     }
-    
     public void setPower(boolean power) {
         this.power = power;
     }
-
+    public boolean isVisited() {
+        return visited;
+    }
+    public void setVisited(boolean visited) {
+        this.visited = visited;
+    }
     @Override
     public String toString() {
         String result = String.valueOf(composant);
@@ -55,4 +54,11 @@ public class Tuile {
         }
         return result;
     }
+
+
+    
+    
+
+
+
 }
